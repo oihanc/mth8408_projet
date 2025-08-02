3b7611fabaacfda0744db015327593dc2cb26030
Oussamamouhtal <oussamamouhtal22@gmail.com>
2025-08-01 21:09:44 -0400

    ajouter lbfgs

--- phase_2/subsolvers.jl
+++ phase_2/subsolvers.jl
@@ -1,6 +1,7 @@
 struct LBFGSStats
     niter::Int
     residuals::Vector{Float64}
+    PAP::Matrix{Float64}
 end
 
 """
@@ -9,26 +10,32 @@ end
 Find the minima of a quadratic model with respect to a trust region. This
 implementation is based on: https://www.gerad.ca/fr/papers/G-2019-64
 """
-function lbfgs(Bj, gk; delta = 0.0, atol=1e-5, rtol=1e-5, mem = 2, itmax = 0, scaling = true)
+function lbfgs(Bj, b; delta = 0.0, atol=1e-26, rtol=1e-26, mem = 2, itmax = 0, scaling = false)
 
+    gk = copy(b)
     dim = length(gk)
-
     gnormk = gnorm0 = norm(gk)
-
+    
     k = 1
     pk = zeros(dim)
     residuals = Float64[gnorm0]
+
+    AP_list = Vector{Vector{Float64}}()   
+    P_list = Vector{Vector{Float64}}()   
+
+    
     if itmax == 0
         itmax = 2*dim
     end
     Hk = InverseLBFGSOperator(dim, mem = mem, scaling = scaling)
 
-    # TODO: review the use of norm(gk) here
     while gnormk > atol + rtol * gnorm0 && k <= itmax
 
         dk = -Hk*gk
-
         bk =  Bj*dk
+        push!(AP_list, bk ./ norm(dk))  
+        push!(P_list, dk ./ norm(dk))  
+
 
         if dot(dk, bk) <= 0
             alphak = -sign(dot(gk, dk))*2*delta/norm(dk)
@@ -36,27 +43,31 @@ function lbfgs(Bj, gk; delta = 0.0, atol=1e-5, rtol=1e-5, mem = 2, itmax = 0, sc
             alphak = -dot(gk, dk)/dot(dk, bk)
         end
 
-        sk = alphak*dk
+        sk = alphak .* dk
         pk = pk + sk
 
         if delta > 0.0 && norm(pk) >= delta
-            pk = pk - sk
+            pk -= sk
             # TODO: optimize implementation
             # compute eq (87) such that norm(pk + tau*sk) = delta (see reference)
             tau = (-dot(pk, sk) + sqrt(dot(pk, sk)^2 + dot(sk, sk)*(delta^2 - dot(pk, pk))))/dot(sk, sk)
 
-            return pk + tau .* sk, LBFGSStats(k, residuals)
+            return pk + tau .* sk
         end
 
-        yk = alphak*bk
+        yk = alphak .* bk
         gk += yk
+        
         gnormk = norm(gk)
         k += 1
-
+        
         # update the inverse Hessian approximation
         push!(Hk, sk, yk)
-        
         push!(residuals, gnormk)
     end
-    return pk,  LBFGSStats(k, residuals)
+    AP  = hcat(AP_list...)   # construit la matrice P de taille (dim, k)
+    P   = hcat(P_list...)     
+    PAP = P' * AP
+    return pk,  LBFGSStats(k, residuals, PAP)
 end
+
