5b308fabcc6fa94f96e278ce5d3bf2c6a72c7041
cordoiha <oihan.cordelier@polymtl.ca>
2025-10-06 13:08:58 -0400

    fix test_krylov.jl

--- test_krylov.jl
+++ test_krylov.jl
@@ -1,6 +1,5 @@
 using Pkg
 Pkg.activate("test_diom_tr")
-Pkg.develop(path="/home/corde/Krylov.jl")   # change path to local Krylov fork
 
 
 using Krylov, LinearAlgebra, SparseArrays, Printf, Random, Test
@@ -9,10 +8,9 @@ import Krylov: solution, statistics, results, elapsed_time,
                solution_count, iteration_count, Aprod_count, Atprod_count,
                issolved, issolved_primal, issolved_dual
 
-include("/home/corde/Krylov.jl/test/test_utils.jl")
+include("Krylov.jl/test/test_utils.jl")
 
-
-# include("/home/corde/Krylov.jl/test/test_cg.jl")
-include("/home/corde/Krylov.jl/test/test_diom.jl")
+include("Krylov.jl/test/test_cg.jl")
+include("Krylov.jl/test/test_diom.jl")
 
 # Pkg.test("Krylov"; coverage=true)
